96c3b547e1f26a6e4ff2ea0409b53967cb3fa111
eric4oo <hou.eric13@gmail.com>
2022-03-15 14:39:14 -0400

    no more gui

--- Chess Engine cpp.cpp
+++ Chess Engine cpp.cpp
@@ -1,45 +1,29 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
-int boardsize = 100;
-cv::Scalar color = cv::Scalar(255, 255, 0);
-
+//Bitboard
+// 
+// 
+//FEN
+//   rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
 
 int main()
 {
-	cv::Mat board = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
-	cv::namedWindow("Mask", 0);
-
-
-	for (int x = 0; x < 8; x++)
-	{
-		for (int y = 0; y < 8; y++)
-		{
-			cv::Point startpoint = cv::Point(100 + x * boardsize, 100 + y * boardsize);
-			cv::Point endpoint = cv::Point(100 + (x + 1) * boardsize, 100 + (y+1) * boardsize);
-
-			if ((x + y) % 2 == 0) 
-			{
-				color = cv::Scalar(62, 148,228);
-
-			}
-			else
-			{
-				color = cv::Scalar(16, 63,120);
-			}
-
-			cout << color << endl;
-
-			cv::rectangle(board, startpoint, endpoint, color, -1, cv::LINE_4);
-		}
-	}
-	cv::imshow("Mask", board);
-	cv::waitKey(0);
 
 	return 0;
+}
+
+int convert_FEN(string FEN)
+{
+	
+}
+
+int writeboard(int board)
+{
+	int boardstate = 0;
+
+	return boardstate;
 }
