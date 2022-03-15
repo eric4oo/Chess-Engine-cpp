2eaa6b687bbde62a7a233d2c223f4b72fd51b0a9
eric4oo <hou.eric13@gmail.com>
2022-03-14 22:05:23 -0400

    ChessBoard

--- Chess Engine cpp.cpp
+++ Chess Engine cpp.cpp
@@ -2,8 +2,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int boardsize = 100;
+cv::Scalar color = cv::Scalar(255, 255, 0);
+
 
 int main()
 {
@@ -15,7 +20,22 @@ int main()
 	{
 		for (int y = 0; y < 8; y++)
 		{
-			cv::rectangle(board, cv::Point(boardsize, 100), cv::Point(200, 200), cv::Scalar(0, 255, 255), -1, cv::LINE_4);
+			cv::Point startpoint = cv::Point(100 + x * boardsize, 100 + y * boardsize);
+			cv::Point endpoint = cv::Point(100 + (x + 1) * boardsize, 100 + (y+1) * boardsize);
+
+			if ((x + y) % 2 == 0) 
+			{
+				color = cv::Scalar(62, 148,228);
+
+			}
+			else
+			{
+				color = cv::Scalar(16, 63,120);
+			}
+
+			cout << color << endl;
+
+			cv::rectangle(board, startpoint, endpoint, color, -1, cv::LINE_4);
 		}
 	}
 	cv::imshow("Mask", board);
